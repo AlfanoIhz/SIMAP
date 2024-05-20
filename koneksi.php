128089a0bee96c5527d7b4af6256d267d5313548
KerinaBakarudin <112407523+KerinaBakarudin@users.noreply.github.com>
2024-05-20 14:18:52 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "simap";
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+if (!$conn){
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>
 
