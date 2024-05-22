19cb762b62b72cc38f3e04dc38497389a46bb3c8
AlfanoIhz <alfanoihza@gmail.com>
2024-05-22 14:35:22 +0700

    ui baru

---
+++ login-adminPerusahaan.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistem Manajemen Aset Perusahaan</title>
+    <link rel="icon" href="assets/simap.png">    <link rel="stylesheet" href="style/style-login.css">
+    <script src="https://kit.fontawesome.com/97216fb713.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <img src="assets/simap.png" class="logo" alt="SIMAP Logo" />
+        <div class="login-box">
+            <h4>Welcome Back. <span>Please Login!</span></h4>
+            <form action="#">
+                <div class="input-box">
+                   <input type="text" name="username"required>
+                   <label>Username</label> 
+                </div>
+                <div class="input-box">
+                   <input type="password" name="password" required >
+                   <label>Password</label> 
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html> 
